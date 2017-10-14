641a00de6c4f75cd5d5baf456e31f5f693d70be7
Paul Aly <paul.aly@outlook.com>
2017-10-14 19:10:30 -0400

    removing old solr config class

--- src/main/java/com/github/maly7/Application.java
+++ src/main/java/com/github/maly7/Application.java
@@ -2,12 +2,8 @@ package com.github.maly7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.github.maly7.data.jpa")
-@EnableSolrRepositories(basePackages = "com.github.maly7.data.solr")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
