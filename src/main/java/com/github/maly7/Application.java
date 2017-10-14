074296fcb3a2b1af06fef7883a4b15e871879b8b
Paul Aly <paul.aly@outlook.com>
2017-10-14 19:20:29 -0400

    explicitly declare repository locations

--- src/main/java/com/github/maly7/Application.java
+++ src/main/java/com/github/maly7/Application.java
@@ -2,8 +2,12 @@ package com.github.maly7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.github.maly7.data.jpa")
+@EnableSolrRepositories("com.github.maly7.data.solr")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
